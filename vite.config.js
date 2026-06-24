27036c88a33c9e272ba19f5f28a2324d859b661c
HARTY8891 <hartyoav@gmail.com>
2026-06-25 02:31:00 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
- base: '/https://harty8891.github.io/BAR/',
+ base: '/https://harty8891.github.io/bar/',
 })
