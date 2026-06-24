209ae6c68fd01f26eeda036fba8b5f8a2282ec4e
HARTY8891 <hartyoav@gmail.com>
2026-06-25 02:34:55 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
- base: '/https://harty8891.github.io/bar/',
+  base: process.env.NODE_ENV === 'production' ? '/bar/' : '/',
 })
